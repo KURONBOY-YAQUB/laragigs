28afdda03013c85cd58d7a6384949ba6fcfcecc8
Yakubov01 <lord17cl@gmail.com>
2022-07-27 21:26:29 +0500

    Using a Layout Component

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,6 +1,4 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
 
 @include('partials._hero')
 @include('partials._search')
@@ -11,4 +9,4 @@
     @endforeach
 </div>    
 
-@endsection
+</x-layout>
